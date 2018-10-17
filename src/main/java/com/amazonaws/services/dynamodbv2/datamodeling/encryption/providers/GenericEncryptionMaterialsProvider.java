d1a79897286fb09fcaae9d762ef9f3604afd586b
John Walker <jhwal@amazon.com>
2018-10-17 16:17:12 -0700

    Add a generic interface for EncryptionMaterialsProvider
    This also moves documentation, which we're going to need to check in a bit..

---
+++ src/main/java/com/amazonaws/services/dynamodbv2/datamodeling/encryption/providers/GenericEncryptionMaterialsProvider.java
@@ -0,0 +1,46 @@
+package com.amazonaws.services.dynamodbv2.datamodeling.encryption.providers;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.encryption.materials.DecryptionMaterials;
+import com.amazonaws.services.dynamodbv2.datamodeling.encryption.materials.EncryptionMaterials;
+
+public interface GenericEncryptionMaterialsProvider<T> {
+    /**
+     * Retrieves encryption materials matching the specified description from some source.
+     *
+     * @param context
+     *      Information to assist in selecting a the proper return value. The implementation
+     *      is free to determine the minimum necessary for successful processing.
+     *
+     * @return
+     *      The encryption materials that match the description, or null if no matching encryption materials found.
+     */
+    public DecryptionMaterials getDecryptionMaterials(T context);
+
+    /**
+     * Returns EncryptionMaterials which the caller can use for encryption.
+     * Each implementation of EncryptionMaterialsProvider can choose its own
+     * strategy for loading encryption material.  For example, an
+     * implementation might load encryption material from an existing key
+     * management system, or load new encryption material when keys are
+     * rotated.
+     *
+     * @param context
+     *      Information to assist in selecting a the proper return value. The implementation
+     *      is free to determine the minimum necessary for successful processing.
+     *
+     * @return EncryptionMaterials which the caller can use to encrypt or
+     * decrypt data.
+     */
+    public EncryptionMaterials getEncryptionMaterials(T context);
+
+    /**
+     * Forces this encryption materials provider to refresh its encryption
+     * material.  For many implementations of encryption materials provider,
+     * this may simply be a no-op, such as any encryption materials provider
+     * implementation that vends static/non-changing encryption material.
+     * For other implementations that vend different encryption material
+     * throughout their lifetime, this method should force the encryption
+     * materials provider to refresh its encryption material.
+     */
+    public void refresh();
+}
